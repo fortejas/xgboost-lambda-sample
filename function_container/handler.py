8dcc6979e0820d936901e0c4e405c4cd7f757cb5
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-09-30 23:18:51 +0000

    Adding tarfile member sanitization to extractall()

--- function_container/handler.py
+++ function_container/handler.py
@@ -27,7 +27,26 @@ def load_model():
     s3.download_file(XGB_MODEL_BUCKET, XGB_MODEL_KEY, model_file)
 
     with tarfile.open(model_file) as tf:
-        tf.extractall(os.path.dirname(model_file))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tf, os.path.dirname(model_file))
 
     return pkl.load(open(os.path.join(os.path.dirname(model_file), 'xgboost-model'), 'rb'))
 
